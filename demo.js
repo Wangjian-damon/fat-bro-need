cc47699e042fe03c779aa398cc85c56746c993be
damonwj <wangjian@tinman.cn>
2018-07-05 17:43:14 +0800

    假吧意思初始化一哈嘛还是

---
+++ demo.js
@@ -0,0 +1,44 @@
+var mysql = require('mysql'); 
+
+var connection = mysql.createConnection({    
+
+  host     : '127.0.0.1',      
+
+  user     : 'damon',             
+
+  password : '5iHGLz0OnQdDHMMU',      
+
+  port: '3306',                  
+
+  database: '111',
+
+});
+ 
+
+connection.connect();
+
+var  userAddSql = 'INSERT INTO my_news_test(id,name,age) VALUES(0,?,?)';
+
+var  userAddSql_Params = ['Wilson', 55];
+
+connection.query(userAddSql,userAddSql_Params,function (err, result) {
+
+        if(err){
+
+         console.log('[INSERT ERROR] - ',err.message);
+
+         return;
+
+        }       
+
+       console.log('-------INSERT----------');
+
+       //console.log('INSERT ID:',result.insertId);       
+
+       console.log('INSERT ID:',result);       
+
+       console.log('#######################'); 
+
+});
+
+connection.end();
